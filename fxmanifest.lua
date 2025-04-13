414b1beab58edabec5675ad200c5adf2fc9ec0fe
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2025-04-13 17:23:49 +1000

    feat(fxmanifest): init dummy data

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author ''
+description ''
+version '0'
+url ''
+
+files {
+  'init.lua',
+  -- 'src/**/shared.lua',
+  -- 'src/**/**/shared.lua',
+  -- 'src/**/**/client.lua'
+}
+
+lua54 'yes'
