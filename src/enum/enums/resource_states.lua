dd0017320fe9b6abd71b0ad1a8b646aa67de115a
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2025-04-18 18:01:55 +1000

    refactor(src/enum/enums/resource_states): use updated enum print

--- src/enum/enums/resource_states.lua
+++ src/enum/enums/resource_states.lua
@@ -1,19 +1,13 @@
 ---@enum resource_states
 return {
-	["invalid"] = {
+	invalid = {
 		[1] = "missing",
 		[2] = "unknown",
 		[3] = "stopped",
-		[4] = "stopping",
-		["missing"] = 1,
-		["unknown"] = 2,
-		["stopped"] = 3,
-		["stopping"] = 4
+		[4] = "stopping"
 	},
-	["valid"] = {
+	valid = {
 		[1] = "started",
-		[2] = "starting",
-		["started"] = 1,
-		["starting"] = 2
+		[2] = "starting"
 	}
 }
