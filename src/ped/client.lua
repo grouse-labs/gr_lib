065bef01fe8a9042cad642d587b3f4fbaeb5db53
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2025-05-01 13:41:52 +1000

    fix(gr_lib/src/ped/client): initial fixes to syncing data

--- src/ped/client.lua
+++ src/ped/client.lua
@@ -26,7 +26,7 @@ end
 function ped.catch(netID, options)
   local obj = {}
   obj.netID = netID
-  obj.entity = NetworkGetNetworkIdFromEntity(netID)
+  obj.entity = NetworkGetEntityFromNetworkId(netID)
   obj.options = {
     data = {},
     ranges = {},
@@ -36,8 +36,19 @@ function ped.catch(netID, options)
     },
     proofs = {}
   }
-  set_ped_options(obj, options)
-  peds[netID] = setmetatable(obj, {__index = ped})
+  -- set_ped_options(obj, options)
+  obj = setmetatable(obj, {__index = ped})
+  local data = options.data
+  if data then
+    if data.max_health then obj:setmaxhealth(data.max_health) end
+    if data.relationship_group and data.relationship_group ~= '' then obj:setrelationshipgroup(data.relationship_group) end
+  end
+  print(json.encode(options))
+  obj:setranges(options.ranges)
+  obj:setcombatai(options.combat_ai)
+  obj:setflags(options.flags?.combat)
+  obj:setproofs(options.proofs)
+  peds[netID] = obj
   return peds[netID]
 end
 
@@ -118,14 +129,15 @@ function ped:setproofs(proofs)
   local entity = self.entity
   SetPedDiesWhenInjured(entity, not proofs.injured or true)
   SetEntityProofs(entity, proofs.bullet or false, proofs.fire or false, proofs.explosion or false, proofs.collision or false, proofs.melee or false, proofs.steam or false, true, proofs.water or false)
-  SetEntityInvincible(entity, proofs.invincible  or false)
+  SetEntityInvincible(entity, proofs.invincible or false)
+  print('proofs', proofs.invincible)
   self.options.proofs = proofs
   return self
 end
 
 --------------------- EVENTS ---------------------
 
-RegisterNetEvent(string.format(EVENT, RESOURCE, CONTEXT, 'ped_initialise'), ped.catch)
-RegisterNetEvent(string.format(EVENT, RESOURCE, CONTEXT, 'ped_destroy'), ped.remove)
+RegisterNetEvent(string.format(EVENT, RESOURCE, 'client', 'ped_initialise'), ped.catch)
+RegisterNetEvent(string.format(EVENT, RESOURCE, 'client', 'ped_destroy'), ped.remove)
 
 return {}
