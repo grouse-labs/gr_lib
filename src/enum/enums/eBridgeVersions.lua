eabe0681166cb580752e17164561436e5f7af54c
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2026-01-15 14:38:08 +1100

    feat(src/enum/enums): init `eBridgeVersions`

---
+++ src/enum/enums/eBridgeVersions.lua
@@ -0,0 +1,24 @@
+return {
+	['callback'] = {
+		['gr_lib'] = "1.0.0",
+		['ox_lib'] = "3.30.6"
+	},
+	['notify'] = {
+		['es_extended'] = "1.13.4",
+		['qb-core'] = "1.3.0"
+	},
+	['framework'] = {
+		['qbx_core'] = "1.23.0",
+		['qb-core'] = "1.3.0",
+		['es_extended'] = "1.13.4"
+	},
+	['lua'] = "5.4",
+	['menu'] = {
+		['qb-menu'] = "1.2.0",
+		['ox_lib'] = "3.30.6"
+	},
+	['target'] = {
+		['ox_target'] = "1.17.2",
+		['qb-target'] = "5.5.0"
+	}
+}
