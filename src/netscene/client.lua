ef2b0450b21c1aeb2604a85fad071d9b3d178060
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2026-02-03 12:35:11 +1100

    fix(src/netscene): `start` runs until scene fin

--- src/netscene/client.lua
+++ src/netscene/client.lua
@@ -48,56 +48,60 @@ function netscene.new(scene_options)
   obj.peds = {}
   obj.objs = {}
 
-  for i = 1, #peds do
-    local ped = peds[i]
-    local entity = ped.entity
-    local model = ped.model
+  if peds then
+    for i = 1, #peds do
+      local ped = peds[i]
+      local entity = ped.entity
+      local model = ped.model
 
-    if not entity then
-      if not model then break end -- error handling
-      if not stream.model(model) then break end -- error handling
-      entity = CreatePed(0, model, coords.x, coords.y, coords.z, 0.0, true, true)
+      if not entity then
+        if not model then break end -- error handling
+        if not stream.model(model) then break end -- error handling
+        entity = CreatePed(0, model, coords.x, coords.y, coords.z, 0.0, true, true)
+      end
+
+      local dict = ped.dict
+
+      if not stream.animdict(dict) then break end -- error handling
+
+      local anim = ped.anim
+      local blend_in = ped.blend_in or 8.0
+      local blend_out = ped.blend_out or -8.0
+      local scene_flags = ped.scene_flags or 0
+      local ragdoll_flags = ped.ragdoll_flags or 0
+      local move_delta = ped.move_delta or 1000.0
+      local ik_flags = ped.ik_flags or 0
+
+      NetworkAddPedToSynchronisedScene(entity, scene, dict, anim, blend_in, blend_out, scene_flags, ragdoll_flags, move_delta, ik_flags)
+      obj.peds[#obj.peds + 1] = entity
     end
-
-    local dict = ped.dict
-
-    if not stream.animdict(dict) then break end -- error handling
-
-    local anim = ped.anim
-    local blend_in = ped.blend_in or 8.0
-    local blend_out = ped.blend_out or -8.0
-    local scene_flags = ped.scene_flags or 0
-    local ragdoll_flags = ped.ragdoll_flags or 0
-    local move_delta = ped.move_delta or 1000.0
-    local ik_flags = ped.ik_flags or 0
-
-    NetworkAddPedToSynchronisedScene(entity, scene, dict, anim, blend_in, blend_out, scene_flags, ragdoll_flags, move_delta, ik_flags)
-    obj.peds[#obj.peds + 1] = entity
   end
 
-  for i = 1, #objs do
-    local object = objs[i]
-    local entity = object.entity
-    local model = object.model
-    local door = object.door or false
+  if objs then
+    for i = 1, #objs do
+      local object = objs[i]
+      local entity = object.entity
+      local model = object.model
+      local door = object.door or false
 
-    if not entity then
-      if not model then break end -- error handling
-      if not stream.model(model) then break end -- error handling
-      entity = CreateObject(model, coords.x, coords.y, coords.z, true, true, door)
+      if not entity then
+        if not model then break end -- error handling
+        if not stream.model(model) then break end -- error handling
+        entity = CreateObject(model, coords.x, coords.y, coords.z, true, true, door)
+      end
+
+      local dict = object.dict
+
+      if not stream.animdict(dict) then break end -- error handling
+
+      local anim = object.anim
+      local blend_in = object.blend_in or 8.0
+      local blend_out = object.blend_out or -8.0
+      local scene_flags = object.scene_flags or 0
+
+      NetworkAddEntityToSynchronisedScene(entity, scene, dict, anim, blend_in, blend_out, scene_flags)
+      obj.objs[#obj.objs + 1] = entity
     end
-
-    local dict = object.dict
-
-    if not stream.animdict(dict) then break end -- error handling
-
-    local anim = object.anim
-    local blend_in = object.blend_in or 8.0
-    local blend_out = object.blend_out or -8.0
-    local scene_flags = object.scene_flags or 0
-
-    NetworkAddEntityToSynchronisedScene(entity, scene, dict, anim, blend_in, blend_out, scene_flags)
-    obj.objs[#obj.objs + 1] = entity
   end
 
   return obj
@@ -107,13 +111,13 @@ function netscene:stop()
   NetworkStopSynchronisedScene(self.nethandle)
 end
 
-function netscene:start()
+function netscene:start(cb, ...)
   local handle = self.nethandle
   NetworkStartSynchronisedScene(handle)
-  while netscene:getphase() < self.stop_phase do
+  repeat
     Wait(100)
-  end
-  netscene:stop()
+    if cb then cb(netscene:getphase(), ...) end
+  until netscene:getphase() >= (self.hold and self.stop_phase or self.stop_phase - 0.01) or netscene:getphase() == 0.0
 end
 
 ---@param peds boolean?
@@ -135,6 +139,9 @@ function netscene:clear(peds, objs)
   end
 end
 
-function netscene:getphase() return GetSynchronizedScenePhase(self.handle) end
+function netscene:getphase()
+  self.handle = IsSynchronizedSceneRunning(self.handle) and self.handle or NetworkGetLocalSceneFromNetworkId(self.nethandle)
+  return GetSynchronizedScenePhase(self.handle)
+end
 
 return netscene.new
